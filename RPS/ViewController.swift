8f4b6355d0e66e8a888f38b3d7c6a24379d523d2
Ednan <52186220+ednanf@users.noreply.github.com>
2023-04-28 21:13:03 -0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Ednan R. Frizzera Filho on 28/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
